9455e848f068f7ae097ce5c8eaa1aeb581c4e8d7
VickyNovarese <mariavirginianovarese@gmail.com>
2024-11-26 23:34:27 +0100

    Selet different styles of menu

--- cypress/e2e/Cypress/select_Menu.cy.js
+++ cypress/e2e/Cypress/select_Menu.cy.js
@@ -1,42 +1,112 @@
-
-describe('✅ToolsQA | Widgets | Dropdown - Select Menu', () => {
+	import { Menu } from "../../support/Page/menu_select_Page";
+	describe("✅ToolsQA | Widgets | Dropdown - Select Menu", () => {
 	beforeEach(() => {
-		cy.visit('https://demoqa.com/select-menu');
+		cy.visit("https://demoqa.com/select-menu");
 	});
 
-	it('Validar seleccionar una opción del select value', () => {
-		cy.get('#withOptGroup').eq(0).click();
-		cy.get('#react-select-2-group-0-heading').click() 
-		cy.get('#withOptGroup').eq(0).should('contain','Group 1, option 1')
-		
+	it("Validar las diferentes opciones del select value", () => {
+		Menu.get.selectMenu().click();
+		Menu.get.group1().should("be.visible").should("have.text", "Group 1");
+		Menu.get.group2().should("be.visible").should("have.text", "Group 2");
+		Menu.get
+		.option1_1()
+		.should("be.visible")
+		.should("have.text", "Group 1, option 1");
+		Menu.get
+		.option1_2()
+		.should("be.visible")
+		.should("have.text", "Group 1, option 2");
+		Menu.get
+		.option2_1()
+		.should("be.visible")
+		.should("have.text", "Group 2, option 1");
+		Menu.get
+		.option2_2()
+		.should("be.visible")
+		.should("have.text", "Group 2, option 2");
+		Menu.get.root().should("be.visible").should("have.text", "A root option");
+		Menu.get
+		.root2()
+		.should("be.visible")
+		.should("have.text", "Another root option");
 	});
-	it('Validar seleccionar una opción del select one', () => {
-		cy.get('#selectOne').click();
-		cy.get('#react-select-3-option-0-1').click(); 
-		cy.get('[class=" css-1uccc91-singleValue"]').contains('Mr.').should('be.visible');
-		
-	});
-	it('Validar seleccionar una opción del Old Style Select Menu', () => {
-		cy.get('#oldSelectMenu').select('Indigo').should('have.value', '8');
-				
-	});
-	it('Validar seleccionar varias opciones del Multiselect drop down', () => {
-		cy.get('.css-tlfecz-indicatorContainer').eq(2).click();
 
-		cy.get('#react-select-4-option-0').click();
-		cy.get('#react-select-4-option-1').click();
-		cy.get('#react-select-4-option-2').click();
-		cy.get('#react-select-4-option-3').click();
-		cy.get('.css-12jo7m5').should('be.visible').should('contain','Red')
-		.should('contain', 'Black')
-		.should('contain', 'Blue')
-		.should('contain','Green')
-		cy.get('.css-1gl4k7y').should('be.visible');
+	it("Validar seleccionar varias opciones del select value", () => {
+		Menu.get.selectMenu().click();
+		Menu.get
+		.option1_1()
+		.should("be.visible")
+		.should("have.text", "Group 1, option 1")
+		.click();
+		Menu.get.selectMenu().click();
+		Menu.get
+		.option2_2()
+		.should("be.visible")
+		.should("have.text", "Group 2, option 2")
+		.click();
+		Menu.get.selectMenu().click();
+		Menu.get.root2().should("have.text", "Another root option").click();
 	});
-	it('Validar seleccionar varias opciones del stantard multiselect', () => {
-		cy.get('#cars').select('volvo').should('be.visible');
-		cy.get('#cars').select('saab').should('be.visible');
-		cy.get('#cars').select('opel').should('be.visible');
-		cy.get('#cars').select('audi').should('be.visible');
+	it("Validar las opciones en el select one", () => {
+		Menu.get.selectOne().click();
+		Menu.get.selectOneOption().then((options) => {
+		expect(options[0]).to.have.text("Dr.");
+		expect(options[1]).to.have.text("Mr.");
+		expect(options[2]).to.have.text("Mrs.");
+		expect(options[3]).to.have.text("Ms.");
+		expect(options[4]).to.have.text("Prof.");
+		expect(options[5]).to.have.text("Other");
+		});
 	});
-});
+
+	it("Validar selecionar las opcion 'Dr.' en el select one", () => {
+		Menu.get.selectOne().click();
+		Menu.get.selectOneOption().then((option) => {
+		option[0].click();
+		});
+	});
+
+	it("Validar selecionar las opcion 'Other' en el select one", () => {
+		Menu.get.selectOne().click();
+		Menu.get.selectOneOption().then((option) => {
+		option[5].click();
+		});
+	});
+
+	it("Validar selecionar las opcion 'Mrs.' en el select one", () => {
+		Menu.get.selectOne().click();
+		Menu.get.selectOneOption().then((option) => {
+		option[2].click();
+		});
+	});
+	it("Validar seleccionar una opción del Old Style Select Menu", () => {
+		Menu.get.oldSelect().then((select) => {
+		cy.wrap(select).select("Red").should("have.value", "red");
+		cy.wrap(select).select("Indigo").should("have.value", "8");
+		cy.wrap(select).select("Aqua").should("have.value", "10");
+		});
+	});
+
+	it("Validar seleccionar varias opciones del Multiselect drop down", () => {
+		Menu.get.multiselect().eq(2).click();
+		Menu.get.multiselectOption().then((options) => {
+		cy.wrap(options[0]).click();
+		cy.wrap(options[1]).click();
+		cy.wrap(options[2]).click();
+		cy.wrap(options[3]).click();
+		});
+		Menu.get
+		.optionsSelected()
+		.should("be.visible")
+		.should("contain", "Red", "Black", "Blue", "Green");
+		Menu.get
+		.noMoreOptions()
+		.should("be.visible")
+		.should("contain", "No options");
+	});
+	it("Validar seleccionar varias opciones del stantard multiselect", () => {
+		Menu.get.autos().then((select) => {
+		cy.wrap(select).select(["volvo", "opel"]).should("be.visible");
+		});
+	});
+	});
--- cypress/support/Page/select_Menu_Page.js
+++ cypress/support/Page/select_Menu_Page.js
@@ -1,7 +1,14 @@
 class menu {
     get={
         selectMenu:()=> cy.get('#withOptGroup'),
-        option1:()=>cy.get('#react-select-2-group-0-heading'),
+        group1:()=>cy.get('#react-select-2-group-0-heading'),
+        group2:()=>cy.get('#react-select-2-group-1-heading'),
+        option1_1:()=>cy.get('#react-select-2-option-0-0'),
+        option1_2:()=>cy.get('#react-select-2-option-0-1'),
+        option2_1:()=>cy.get('#react-select-2-option-1-0'),
+        option2_2:()=>cy.get('#react-select-2-option-1-1'),
+        root:()=>cy.get('#react-select-2-option-2'),
+        root2:()=>cy.get('#react-select-2-option-3'),
         about:()=>cy.get('#about_sidebar_link.bm-item.menu-item'),
         logout:()=>cy.get('#logout_sidebar_link'),
         reset:()=>cy.get('#reset_sidebar_link'),
